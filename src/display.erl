f576106e5d92c00b3457a238c94cac9bdb6f5ade
dennyabraham <dennysabraham@gmail.com>
2010-05-07 15:17:45 -0500

    added board and crossout display routines

--- src/display.erl
+++ src/display.erl
@@ -8,8 +8,85 @@
 
 -module (display).
 
--export ([board/1]).
+-export ([board/1, board/2, show_board/2, show_board/1]).
 
-% board({{A1,A2,A3},{B1,B2,B3},{C1,C2,C3}}) ->
-board({{_,_,_}, {_,_,_}, {_,_,_}}) ->
-    " - | - | - ~n---+---+---~n  - | - | - ~n---+---+---~n - | - | - ".
+
+show_board(Rows, Matches) -> 
+    io:format("~n"),
+    io:format(board(Rows, Matches)).
+show_board(Rows) ->
+    io:format("~n"),
+    io:format(board(Rows)).
+    
+board({InputRow1, InputRow2, InputRow3}, {diagonal, Matches}) ->
+    OutputRow1 = cross_out_diagonal_matches(InputRow1, 1, Matches),
+    OutputRow2 = cross_out_diagonal_matches(InputRow2, 2, Matches),
+    OutputRow3 = cross_out_diagonal_matches(InputRow3, 3, Matches),
+    board({OutputRow1, OutputRow2, OutputRow3});    
+board({InputRow1, InputRow2, InputRow3}, {vertical, Matches}) ->
+    OutputRow1 = cross_out_vertical_matches(InputRow1, Matches),
+    OutputRow2 = cross_out_vertical_matches(InputRow2, Matches),
+    OutputRow3 = cross_out_vertical_matches(InputRow3, Matches),
+    board({OutputRow1, OutputRow2, OutputRow3});    
+board({InputRow1, InputRow2, InputRow3}, {horizontal, Matches}) ->
+    OutputRow1 = cross_out_horizontal_matches(InputRow1, 1, Matches),
+    OutputRow2 = cross_out_horizontal_matches(InputRow2, 2, Matches),
+    OutputRow3 = cross_out_horizontal_matches(InputRow3, 3, Matches),
+    board({OutputRow1, OutputRow2, OutputRow3}).
+    
+board({{"","",""},{"","",""},{"","",""}}) ->
+    board({{" "," "," "},{" "," "," "},{" "," "," "}});
+board({{A1,A2,A3}, {B1,B2,B3}, {C1,C2,C3}}) ->
+    " "++ A1 ++ " | " ++ A2 ++ " | " ++ A3 ++ " ~n---+---+---~n " ++ 
+          B1 ++ " | " ++ B2 ++ " | " ++ B3 ++ " ~n---+---+---~n " ++ 
+          C1 ++ " | " ++ C2 ++ " | " ++ C3 ++ " ".
+          
+crossed_out_row() -> {"-", "-", "-"}.    
+
+cross_out_horizontal_matches(InputRow, RowNumber, Matches) ->
+    case lists:member(RowNumber, Matches) of
+        true -> crossed_out_row();
+        false -> InputRow
+    end.     
+     
+cross_out_vertical_matches({Col1, Col2, Col3}, Matches) ->
+    OutputCol1 = cross_out_vertical_matches(Col1, 1, Matches),
+    OutputCol2 = cross_out_vertical_matches(Col2, 2, Matches),
+    OutputCol3 = cross_out_vertical_matches(Col3, 3, Matches),
+    {OutputCol1, OutputCol2, OutputCol3}.
+    
+cross_out_vertical_matches(Column, ColNumber, Matches) ->
+    case lists:member(ColNumber, Matches) of
+        true ->  "|";
+        false -> Column
+    end.   
+    
+cross_out_diagonal_matches({Col1, Col2, Col3}, RowNumber, Matches) ->    
+    OutputCol1 = cross_out_diagonal_matches(Col1, RowNumber, 1, Matches),
+    OutputCol2 = cross_out_diagonal_matches(Col2, RowNumber, 2, Matches),
+    OutputCol3 = cross_out_diagonal_matches(Col3, RowNumber, 3, Matches),
+    {OutputCol1, OutputCol2, OutputCol3}.
+    
+cross_out_diagonal_matches(Column, _, _, []) -> Column;
+cross_out_diagonal_matches(Column, RowNumber, ColNumber, [FirstMatch | OtherMatch]) ->
+    case FirstMatch of
+        1 -> 
+            Symbol = crossed_out_diagonal_for(Column, RowNumber, ColNumber, FirstMatch),
+            cross_out_diagonal_matches(Symbol, RowNumber, ColNumber, OtherMatch);
+        2 -> 
+            Symbol = crossed_out_diagonal_for(Column, RowNumber, ColNumber, FirstMatch),
+            cross_out_diagonal_matches(Symbol, RowNumber, ColNumber, OtherMatch);        
+        _ -> Column
+    end.
+    
+crossed_out_diagonal_for(_, 1, 1, 1) -> "\\";
+crossed_out_diagonal_for(_, 2, 2, 1) -> "\\";
+crossed_out_diagonal_for(_, 3, 3, 1) -> "\\";
+crossed_out_diagonal_for(_, 3, 1, 2) -> "/";
+crossed_out_diagonal_for(_, 2, 2, 2) -> "/";
+crossed_out_diagonal_for(_, 1, 3, 2) -> "/";
+crossed_out_diagonal_for(Column, _, _, _) -> Column.
+
+
+
+
--- test/display_test.erl
+++ test/display_test.erl
@@ -15,9 +15,59 @@
 tests() -> 
     [ 
         it_should("construct an empty board", 
-            {display:board({{e, e, e},{e, e, e},{e, e, e}}), should(equal(empty_board()))}
+            {display:board({{"", "", ""},{"", "", ""},{"", "", ""}}), should(equal(empty_board()))}
+        ),
+        
+        it_should("construct an arbitrary board", 
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}}), should(equal(arbitrary_board()))}
+        ),
+        
+        it_should("cross out all horizontal matches",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{horizontal, [1,2,3]}), should(equal(horizontal_rows()))}
+        ),
+        
+        it_should("cross out only what matches horizontally",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{horizontal, [1,3]}), should(equal(rows_1_and_3()))}
+        ),
+        
+        it_should("cross out all vertical matches",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{vertical, [1,2,3]}), should(equal(vertical_matches()))}
+        ),
+
+        it_should("cross out only what matches vertically",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{vertical, [1,2]}), should(equal(cols_1_and_2()))}
+        ),
+        
+        it_should("cross out all diagonal matches",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{diagonal, [1,2]}), should(equal(diagonal_matches()))}
+        ),
+        
+        it_should("cross out only what matches diagonally",
+            {display:board({{"a","b", "c"},{"d", "e", "f"},{"g", "h", "i"}},{diagonal, [2]}), should(equal(diag_2()))}
         )
     ].
     
 empty_board() -> 
-    " - | - | - ~n---+---+---~n  - | - | - ~n---+---+---~n - | - | - ".
+    "   |   |   ~n---+---+---~n   |   |   ~n---+---+---~n   |   |   ".
+
+arbitrary_board() -> 
+    " a | b | c ~n---+---+---~n d | e | f ~n---+---+---~n g | h | i ".
+    
+horizontal_rows() -> 
+    " - | - | - ~n---+---+---~n - | - | - ~n---+---+---~n - | - | - ".
+    
+rows_1_and_3() -> 
+    " - | - | - ~n---+---+---~n d | e | f ~n---+---+---~n - | - | - ".
+
+vertical_matches() -> 
+    " | | | | | ~n---+---+---~n | | | | | ~n---+---+---~n | | | | | ".
+    
+cols_1_and_2() ->
+    " | | | | c ~n---+---+---~n | | | | f ~n---+---+---~n | | | | i ".
+
+diagonal_matches() ->
+    " \\ | b | / ~n---+---+---~n d | / | f ~n---+---+---~n / | h | \\ ".
+    
+diag_2() -> 
+    " a | b | / ~n---+---+---~n d | / | f ~n---+---+---~n / | h | i ".
+    
